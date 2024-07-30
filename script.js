27449cde1651aeca147548d738a3f548801b25f8
Cadz360 <173970617+Cadz360@users.noreply.github.com>
2024-07-30 07:43:25 -0700

    feat: user choose grids from 1 - 100

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const squaresInput = document.querySelector("input")
 
-for (i = 0; i < 16; i++){
-    const column = document.createElement("div")
-    column.classList.add("column");
-    for(let j = 0; j < 16; j++) {
-        const row = document.createElement("div");
-        row.classList.add("row")
-        column.appendChild(row)
+squaresInput.addEventListener("change", () => {
+    for (let i = 0; i < squaresInput.value; i++){
+        const column = document.createElement("div")
+        column.classList.add("column");
+        for(let j = 0; j < squaresInput.value; j++) {
+            const row = document.createElement("div");
+            row.classList.add("row")
+            column.appendChild(row)
+        }
+        sketchContainer.appendChild(column)
     }
-    sketchContainer.appendChild(column)
+    //remove all divs when input has been changed
 
-}   
+})
 
 const grids = document.querySelectorAll(".row")
 
