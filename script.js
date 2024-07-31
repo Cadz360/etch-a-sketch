89d8c9f77eaa2771d165a7176908ed49fb3600c8
Cadz360 <173970617+Cadz360@users.noreply.github.com>
2024-07-31 08:26:40 -0700

    feat: every time user hovers over each grid that already has a color, it increases its opacity by 10%

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const randomRGB = () => {
         r = Math.random,
         s = 255;
     return 'rgb(' + o(r()*s) + ',' 
-    + o(r()*s) + ',' + o(r()*s) + ')';
+    + o(r()*s) + ',' + o(r()*s) + ',' + 0.1 +')';
 }
 
 
@@ -18,16 +18,29 @@ squaresInput.addEventListener("change", () => {
         for(let j = 0; j < squaresInput.value; j++) {
             const row = document.createElement("div");
             row.classList.add("row")
-            column.appendChild(row)
-            row.addEventListener('mouseover', () => {
+            column.appendChild(row);
+            row.addEventListener('mouseenter', () => {
                 row.style.backgroundColor = randomRGB()
-                console.log(row.style.backgroundColor)
-                //keep color that has been initially replaced each grid to from white
-                // one option could be manipulating css through dom
-                // another option could be making a variable (false default) and turns into true once user triggers mouseover so listener will not trigger again
-                //darken grid each mouseover on the same grid
-            }, { once: true})
+            }, { once: true});
+            const controller = new AbortController()
+            let bgOpacity = 0.1;
+            row.addEventListener('mouseenter', () => {
+                //This event listener only fires after background color for each grid has been set
+                //Each time this listener gets fired, it will get the value of the BG color of each grid
+                //Then, increment opacity by 0.1 or 10% up to max opacity
+                if (bgOpacity >= 0.2 && bgOpacity <= 1) {
+                    console.log(Math.round(bgOpacity * 10) / 10)
+                    let newOpacity = Math.round(bgOpacity * 10) / 10
+                    let oldOpacity = Math.round((bgOpacity - 0.1) * 10) / 10
+                    let bgColor = row.style.backgroundColor
+                    row.style.backgroundColor = bgColor.replace(`${oldOpacity}`, `${newOpacity}`)
+                } else if(bgOpacity > 1) {
+                    controller.abort()
+                }
+                bgOpacity += 0.1
+            }, {signal: controller.signal})
+        }
         sketchContainer.appendChild(column)
     }
     }
-})
+)
